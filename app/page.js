0057ec3cc7520e97767b926e24966b10b942a74f
unknown <binitkaser@gmail.com>
2024-01-15 20:18:13 +0530

    add scroll div

--- app/page.js
+++ app/page.js
@@ -2,8 +2,19 @@
 
 export default function Home() {
   return (
-    <div className='flex items-center justify-center h-screen'>
+    <div className='flex flex-col items-center justify-center h-screen'>
       <h1 className='font-bold text-4xl'>Hello world G !!</h1>
+      <div className="w-[30%] h-[300px] overflow-y-auto border-2 mt-5 bg-gray-400 rounded-md"><p className="p-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis quo facere explicabo fugit laborum minima autem. Officia adipisci voluptatum autem ratione aliquam expedita eligendi, reiciendis laborum. Omnis, aspernatur inventore, eaque labore in voluptatibus porro eveniet, quibusdam magni harum sapiente nam facere 
+        voluptas nihil animi molestias. Maiores nam deleniti minus delectus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi iusto, placeat alias deleniti reiciendis est dolorum vel facilis tenetur tempore nihil dolores veniam recusandae. Corporis ipsum inventore atque assumenda, quia asperiores pariatur reprehenderit eum debitis! Numquam sapiente sed eligendi veniam, ducimus ipsum, dolore ipsam alias vero repellat natus labore quisquam iusto? Maiores optio, 
+        laudantium culpa ad facilis sequi maxime illum asperiores harum iusto autem numquam eos voluptate cum excepturi! Quos corporis sed eligendi rem et quaerat maxime tempora laboriosam, 
+        quo sit minima a? Nemo esse iusto, neque numquam molestias possimus sapiente cum, ducimus asperiores qui itaque ab natus nobis hic alias ipsum reiciendis quasi quis aspernatur impedit excepturi, nostrum officiis. 
+        Consequuntur quaerat sapiente cum iste eius, vitae necessitatibus. Aliquam sit qui iure, repudiandae saepe perspiciatis ipsum. Dicta, labore incidunt. 
+        Earum debitis voluptas facere quam incidunt ipsam. Reprehenderit, sint facere doloribus saepe explicabo ea reiciendis maiores neque assumenda officiis veritatis possimus, tenetur cumque officia! Tempora repudiandae reiciendis, perspiciatis sit quod aliquid vero! Eos perferendis totam dolorem beatae animi nemo, corrupti et 
+        voluptatem magni quasi explicabo, qui incidunt, minima atque inventore aperiam facilis eligendi accusamus quis sapiente voluptates 
+        laudantium. Provident iusto molestias eaque eius, autem voluptatibus laudantium ea tempore amet aspernatur natus vero corrupti ab odio, 
+        earum repudiandae ut officiis deleniti maiores quisquam eligendi quos harum! Facere amet recusandae in id labore eos natus error eaque, obcaecati aut tempore, dolorem rerum tempora maiores molestiae, eligendi voluptate dolorum veritatis quos molestias dolores blanditiis optio. Dolore veritatis ea ad porro, ipsam temporibus facilis necessitatibus atque odio quidem amet quae quam recusandae explicabo eum? Numquam consequatur dolorem dolores a, corporis in pariatur provident 
+        autem reprehenderit eligendi vero optio cumque, cupiditate rem! Aliquid veritatis repudiandae officiis labore atque quam rem, non laborum 
+        velit minus excepturi architecto assumenda incidunt ipsa, necessitatibus saepe consequatur eum asperiores at eligendi?</p></div>
     </div>
   )
 }
